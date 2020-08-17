9e47020692087257d729f1894e2824f134d6461e
ramdhanll <bscdhani30@gmail.com>
2020-08-17 21:21:13 +0700

    a

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios'
+// import axios from 'axios'
 
 // untuk develepment
 // axios.defaults.baseURL = 'http://localhost:5000';
